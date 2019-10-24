1477c5eb1eda477ccc2b92129fa66e2b204697d2
n-jeremic <56317674+n-jeremic@users.noreply.github.com>
2019-10-24 23:02:51 +0200

    Add files via upload

--- Untitled-2.js
+++ Untitled-2.js
@@ -6,10 +6,13 @@ document.querySelector(".dropdown-content").addEventListener("click", favouriteM
 window.addEventListener("load", displayLikes);
 window.addEventListener("hashchange", function () {
     if (location.href === "file:///C:/Users/Nikola/Desktop/practice/Untitled-1.html") {
-        clearUI();
         document.querySelector(".search_field").value = null;
+        clearUI();
     }
-});
+    let hash = location.hash.slice(1);
+    let title = hash.replace(/%20/g, " ");
+    hash1(title);
+ });
 
 // STORE THE ARRAY
 let movieInfo;
@@ -131,13 +134,13 @@ function displayTitle(el, index) {
 
 function displayInfo(event) {
 
-    location.hash = "#part1";
-
     // GET THE VALUE OF THE TARGET ELEMENT
     let id = event.target.id;
     if (event.target.parentNode.id !== "flex-container-right" && event.target.parentNode.id !== "flex-container-left") {
     let splitArr = document.getElementById(`${id}`).childNodes[0].nodeValue.split(" - ");
     let value = splitArr[0];
+
+    location.hash = `${value}`;
 
     // CLEAR THE USER INTERFACE
     document.getElementById("middle1").innerHTML = "";
@@ -184,8 +187,6 @@ function generateImgURL(url, size, path) {
 // IMPLEMENTING MOVIE SEARCH
 async function movieSearch() {
 
-    location.hash = "#part1";
-
     // READ VALUE FROM THE SEARCH FIELD
     let query = document.querySelector(".search_field").value.toString();
     
@@ -197,6 +198,7 @@ async function movieSearch() {
         let movieJSON = await movie.json();
 
         let id = movieJSON.results[0].id;
+        let title = movieJSON.results[0].title;
 
         getDetails(id).then(res => {
             let markUp = `
@@ -222,6 +224,8 @@ async function movieSearch() {
                 addToFavourites(id);
             });
             document.getElementById("close").addEventListener("click", clearUI);
+
+            location.hash = `${title}`;
 
         // CHECK IF REQUIRED MOVIE IS IN FAVOURITES
         let ids = movieInfo.map(el => el.id);
@@ -363,12 +367,13 @@ function removeFromFavourites() {
 
 
 function favouriteMovieInfo(event) {
-
-    location.hash = "#part1"
     
     if (event.target.id !== "drop") {
 
     let value = event.target.childNodes[0].nodeValue;
+
+    location.hash = `${value}`;
+
     let titles = movieInfo.map(el => el.name);
     let index = titles.findIndex(el => el === value);
     let markUp =  `
@@ -416,4 +421,80 @@ function displayLikes() {
     likes = likesArr;
     return likes;
 
+}
+
+async function hash1(title) {
+    let titlesArr = movieInfo.map(el => el.name);
+    let index = titlesArr.findIndex(el => el === title);
+    
+    let array = document.querySelector(".movie-title").childNodes[0].nodeValue.split(" - ");
+    let title1 = array[0];
+
+    if (title !== title1 && index !== -1) {
+        let markUp = `
+        <div id="middle-container">
+        <div class="image1"><img class="image" src="${movieInfo[index].imgURL}" alt="${movieInfo[index].name}"></div>
+                <div class="movie-info">
+                    <h1 class="movie-title">${movieInfo[index].name} - ${movieInfo[index].year}</h1>
+                    <p><strong>GENRE:</strong> ${movieInfo[index].genres}</p>
+                    <p class="rating"><strong>RATING:</strong> ${movieInfo[index].rating} (${movieInfo[index].ratingCount} votes)</p>
+                    <p class="crew"><strong>CREW:</strong> ${movieInfo[index].crew}</p>
+                    <p class="cast"><strong>CAST:</strong> ${movieInfo[index].cast}</p>
+                    <b><span id="fav-text" style="color:red"></span></b>
+                    <p id="rem-fav"></p>
+                    <button class="favourites">Add to favourites</button><button id="close">Close window</button>
+                </div>
+            <div class="description">
+               <p><strong>OVERVIEW:</strong> ${movieInfo[index].overview}</p>
+        </div>
+        </div>`;
+        document.getElementById("middle1").innerHTML = markUp;
+        document.getElementById("close").addEventListener("click", clearUI);
+        document.querySelector(".favourites").addEventListener("click", addToFavourites);
+        
+        if (movieInfo[index].favourite === true) {
+            document.querySelector(".favourites").remove();
+            document.getElementById("fav-text").innerHTML = "Added to favourites!";
+            document.getElementById("rem-fav").innerHTML = `<button id="rem">Remove from favourites</button>`;
+            document.getElementById(`rem`).addEventListener("click", removeFromFavourites);
+        }
+    } else if (title !== title1 && index === -1) {
+        try {
+            document.getElementById("middle1").innerHTML = "";
+            let movie = await fetch (`https://api.themoviedb.org/3/search/movie?api_key=821e6e287624c7921335f083519db105&language=en-US&query=${title}&page=1&include_adult=false`)
+            let movieJSON = await movie.json();
+    
+            let id = movieJSON.results[0].id;
+
+            getDetails(id).then(res => {
+                let markUp = `
+                <div id="middle-container">
+                <div class="image1"><img class="image" src="${res.imgURL}" alt="${res.name}"></div>
+                        <div class="movie-info">
+                            <h1 class="movie-title">${res.name} - ${res.year}</h1>
+                            <p><strong>GENRE:</strong> ${res.genres}</p>
+                            <p class="rating"><strong>RATING:</strong> ${res.rating} (${res.ratingCount} votes)</p>
+                            <p class="crew"><strong>CREW:</strong> ${res.crew}</p>
+                            <p class="cast"><strong>CAST:</strong> ${res.cast}</p>
+                            <b><span id="fav-text" style="color:red"></span></b>
+                            <p id="rem-fav"></p>
+                            <button class="favourites">Add to favourites</button><button id="close">Close window</button>
+                        </div>
+                    <div class="description">
+                       <p><strong>OVERVIEW:</strong> ${res.overview}</p>
+                </div>
+                </div>`;
+    
+                document.getElementById("middle1").innerHTML = markUp;
+                document.querySelector(".favourites").addEventListener("click", function () {
+                    addToFavourites(id);
+                });
+                document.getElementById("close").addEventListener("click", clearUI);
+            })
+            } catch(error) {
+                document.getElementById("middle1").innerHTML = "";
+                let markUp = `<h1 id="error">NO RESULTS FOUND.</h1>`;
+                document.getElementById("middle1").innerHTML = markUp;
+            }    
+    }
 }
